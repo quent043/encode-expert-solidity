040ac99e1546c464ee539a99574d0e8532452665
Quentin D.C <quent043@hotmail.com>
2023-05-02 11:20:32 +0100

    Homework5 Assembly ex 4

---
+++ Homework5/assembly/Assembly_4.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.4;
+
+contract Scope {
+    uint256 public count = 10;
+
+    function increment(uint256 num) public {
+        // Modify state of the count variable from within
+        // the assembly segment
+        assembly {
+            let count_slot := sload(0x00)
+            sstore(count_slot,11)
+        }
+    }
+}

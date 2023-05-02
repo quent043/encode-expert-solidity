c6a4f09808ad62c6ba1c681d84998c6203b88234
Quentin D.C <quent043@hotmail.com>
2023-05-02 15:50:52 +0100

    Fixed mistake in ex1

--- Homework5/assembly/Assembly_1.sol
+++ Homework5/assembly/Assembly_1.sol
@@ -1,25 +1,25 @@
 pragma solidity ^0.8.4;
 
 contract Intro {
-    function intro() public pure returns (uint16) {
+    function intro() public pure returns (uint16 result) {
         uint256 mol = 420;
 
         // Yul assembly magic happens within assembly{} section
         assembly {
-            // stack variables are instantiated with
-            // let variable_name := VALßUE
-            // instantiate a stack variable that holds the value of mol
-            // To return it needs to be stored in memory
-            // with command mstore(MEMORY_LOCATION, STACK_VARIABLE)
-            // to return you need to specify address and the size from the starting point
+        // stack variables are instantiated with
+        // let variable_name := VALßUE
+        // instantiate a stack variable that holds the value of mol
+        // To return it needs to be stored in memory
+        // with command mstore(MEMORY_LOCATION, STACK_VARIABLE)
+        // to return you need to specify address and the size from the starting point
             let y := mol
             let nextFreeMemoryLocation := 0x40
-            mstore(mload(nextFreeMemoryLocation), y)
-            //Update free memory pointer
+            mstore(nextFreeMemoryLocation, y)
+            result := mload(0x40)
+        //Update free memory pointer
             mstore(0x40, add(nextFreeMemoryLocation, 0x20))
-            //Can we do that?
-//            mstore(0x40, add(nextFreeMemoryLocation, mol))
-            return(0x40, 0x20)
+        //Can we do that?
+        //            mstore(0x40, add(nextFreeMemoryLocation, mol))
         }
     }
 }
